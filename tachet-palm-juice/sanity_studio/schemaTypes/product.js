ee5462d1bf371c97e4d953a9924c9cdc7bfff185
Mike-Vidal <113799147+Mike-Vidal@users.noreply.github.com>
2024-06-07 11:28:54 -0400

    Created schema and Initialised database with sanity

---
+++ tachet-palm-juice/sanity_studio/schemaTypes/product.js
@@ -0,0 +1,44 @@
+import {defineType, defineField} from 'sanity'
+
+export const product = defineType({
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'array',
+      of: [{type: 'image'}],
+      options: {
+        hostpot: 'true',
+      },
+    }),
+    defineField(
+      {
+        name: 'name',
+        title: 'Name',
+        type: 'string',
+      },
+      {
+        name: 'slug',
+        title: 'Slug',
+        type: 'slug',
+        option: {
+          source: 'name',
+          maxlenght: 90,
+        },
+      },
+    ),
+    defineField({
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    }),
+    defineField({
+      name: 'details',
+      title: 'Details',
+      type: 'string',
+    }),
+  ],
+})
